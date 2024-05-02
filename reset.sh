93f0a3222080b1ef52fb073e614cccd8c27eec76
kijungle <kijung982@gmail.com>
2024-05-02 12:33:27 +0900

    FIX: directory tree

--- reset.sh
+++ reset.sh
@@ -1,3 +1,8 @@
+kubectl patch pv mysql-pv -p '{"metadata":{"finalizers":null}}' -n database-ns
+kubectl patch pvc mysql-pvc -p '{"metadata":{"finalizers":null}}' -n database-ns
+kubectl patch pv mongo-pv -p '{"metadata":{"finalizers":null}}' -n database-ns
+kubectl patch pvc mongo-pvc -p '{"metadata":{"finalizers":null}}' -n database-ns
+
 kubectl delete namespace frontend-ns
 kubectl delete namespace backend-ns
 kubectl delete namespace database-ns
