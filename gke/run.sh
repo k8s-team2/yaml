274595edd503e815395ea39d1b0ec14d0dceef6f
kijungle <kijung982@gmail.com>
2024-05-03 22:59:18 +0900

    FIX: Directory System

--- gke/run.sh
+++ gke/run.sh
@@ -18,9 +18,4 @@ if kubectl get namespace monitoring-ns >/dev/null 2>&1; then
     kubectl delete namespace database-ns
 fi
 
-kubectl create ns frontend-ns
-kubectl create ns backend-ns
-kubectl create ns database-ns
-kubectl create ns monitoring-ns
-
-kubectl apply -f .
+k apply -f ./etc/namespace.yaml
