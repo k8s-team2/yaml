5503d8629f36f7ee56460227f5acab3a65f19617
kijungle <kijung982@gmail.com>
2024-05-04 15:13:59 +0900

    ADD: stop.sh

---
+++ gke/stop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if kubectl get namespace frontend-ns >/dev/null 2>&1; then
+    kubectl delete namespace frontend-ns
+fi
+
+if kubectl get namespace backend-ns >/dev/null 2>&1; then
+    kubectl delete namespace backend-ns
+fi
+
+if kubectl get namespace database-ns >/dev/null 2>&1; then
+    kubectl patch pvc mysql-pvc -p '{"metadata":{"finalizers":null}}' -n database-ns
+    kubectl patch pvc mongo-pvc -p '{"metadata":{"finalizers":null}}' -n database-ns
+    kubectl delete namespace database-ns
+fi
+
+if kubectl get namespace monitoring-ns >/dev/null 2>&1; then
+    kubectl patch pvc monitoring-pvc -p '{"metadata":{"finalizers":null}}' -n monitoring-ns
+    kubectl delete namespace database-ns
+fi
